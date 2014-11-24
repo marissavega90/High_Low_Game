34058c01f045589407c3c10b28454c2fcb2899a6
Marissa <marissavega90@gmail.com>
2014-11-24 10:38:13 -0600

    Added COUNTER

--- high_low.php
+++ high_low.php
@@ -1,6 +1,10 @@
 <?php
 $number = rand(1 , 100);
 
+$counter = 0;
+
+$guess = 0;
+
 do {
     //code goes here!
 
@@ -8,19 +12,23 @@ do {
 
     $guess = fgets(STDIN);
 
-    if ($guess > $number) {
-        echo "LOWER \n";
-    }
+        if ($guess > $number) {
+            echo "LOWER \n";
+        }
 
-    elseif ($guess < $number) {
-        echo "HIGHER \n";
-    }
-   
+        elseif ($guess < $number) {
+            echo "HIGHER \n";
+        }
+
+    $counter++;
+
    //code stops here! 
 
 } while ($guess != $number);
 
-echo 'Good guess!';
+echo "Good guess! \n";
+
+echo "Number of guesses: $counter\n";
 
 exit(0)
 
